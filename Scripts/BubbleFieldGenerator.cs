79552a86ae8d87141f2ad129c5a688761affe11d
oksenofontov <oksenofontov@naumen.ru>
2025-01-25 02:56:34 +0500

    insctructions ready 2 types:

--- Scripts/BubbleFieldGenerator.cs
+++ Scripts/BubbleFieldGenerator.cs
@@ -13,17 +13,37 @@ public class BubbleFieldGenerator : MonoBehaviour
     // Префабы для объектов
     public GameObject bubbleObject;
 
+    public SpriteRenderer instructionSpriteObject;
+    public int currentCorrectPopsIndex = 0;
     public Sprite insrtuctionWrongSprite;
     public Sprite insrtuctionRightSprite;
+    public Sprite[] instructionSprites;
+
+    private readonly int[][,] _correctPops =
+    {
+        new[,]
+        {
+            { 0, 0, 1, 1, 1, 1 },
+              { 0, 1, 0, 0, 1, 0 },
+            { 0, 0, 1, 0, 0, 1 },
+              { 0, 1, 0, 0, 1, 0 },
+            { 0, 0, 0, 0, 0, 0 },
+        },
+        new[,]
+        {
+            { 0, 0, 0, 0, 1, 1 },
+                { 0, 0, 1, 1, 1, 0 },
+            { 0, 0, 1, 1, 1, 0 },
+                { 1, 1, 1, 0, 0, 0 },
+            { 1, 1, 0, 0, 0, 0 },
+        },
+    };
 
     // Радиус гексагона (расстояние между центрами соседних гексагонов)
     private float hexRadius;
 
     // Время между обновлениями поля (в секундах)
     public float updateInterval = 3.0f;
-
-    // Поле данных
-    private int[,] currentField;
 
     private GameObject currentBubbleField = null;
 
@@ -39,16 +59,6 @@ public class BubbleFieldGenerator : MonoBehaviour
             }
         }
 
-        // Пример начального поля
-        currentField = new int[,]
-        {
-            { 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 1, 0, 0, 0 },
-            { 0, 0, 1, 1, 1, 0 },
-            { 0, 0, 1, 1, 0, 0 },
-            { 0, 0, 0, 0, 0, 0 },
-        };
-
         // Запуск корутины
         StartCoroutine(GenerateFieldPeriodically());
     }
@@ -61,7 +71,7 @@ public class BubbleFieldGenerator : MonoBehaviour
         while (true)
         {
             // Генерируем поле
-            GenerateField(currentField);
+            GenerateField(_correctPops[currentCorrectPopsIndex]);
 
             // Ждём указанное время
             yield return new WaitForSeconds(updateInterval);
@@ -79,7 +89,7 @@ public class BubbleFieldGenerator : MonoBehaviour
             Destroy(currentBubbleField);
         }
 
-        currentBubbleField = Instantiate(bubbleField, transform.position, Quaternion.identity);
+        currentBubbleField = Instantiate(bubbleField, transform.position + new Vector3(0f, 0f, -1f), Quaternion.identity);
 
         int rows = field.GetLength(0);
         int cols = field.GetLength(1);
@@ -121,11 +131,25 @@ public class BubbleFieldGenerator : MonoBehaviour
                 // Создаем объект и добавляем его в иерархию
                 Vector3 position = currentBubbleField.transform.position + new Vector3(xPos, yPos, 0);
                 Instantiate(prefabToInstantiate, position, Quaternion.identity, currentBubbleField.transform);
+
+                if (!isConvayer)
+                {
+                    instructionSpriteObject.sprite = instructionSprites[currentCorrectPopsIndex];
+                }
             }
+        }
+
+        if (currentCorrectPopsIndex + 1 >= _correctPops.Length)
+        {
+            currentCorrectPopsIndex = 0;
+        }
+        else
+        {
+            currentCorrectPopsIndex++;
         }
     }
 
-    // // Получаем SpriteRenderer дочернего объекта с тегом "background"
+    // Получаем SpriteRenderer дочернего объекта с тегом "background"
     // SpriteRenderer GetChildSpriteRendererWithTag(GameObject parent, string tag)
     // {
     //     // Находим всех дочерних объектов
